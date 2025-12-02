f13db4c781c6224619d3895dd2d864b64fe8587e
Chi-Kit Pao <Chi-KitPao@gmx.de>
2025-12-02 23:52:59 +0100

    Added Solution for AoC 2025, Day 2, Part 2

--- 2025/Day02/Day02.ps1
+++ 2025/Day02/Day02.ps1
@@ -5,8 +5,7 @@
     pwsh -ExecutionPolicy ByPass -File "Day02.ps1"
 #>
 
-
-function AddInvalidIds ($Low, $High) {
+function AddInvalidIds1 ($Low, $High) {
     [long]$Answer = 0
     [long]$i = 1
     for (; $i -le $High; $i++) {
@@ -23,23 +22,69 @@ function AddInvalidIds ($Low, $High) {
 }
 
 function Main {
+    [long]$MaxValue = 0
     $Line = Get-Content "input.txt" -First 1
     $Ranges = $Line -split "[,]"
+    
+    # Part 1
     [long]$Answer1 = 0
     foreach ($Range in $Ranges) {
         $Boundaries = $Range -split "[-]"
         # REMARK: int was too small
         $L = [long]$Boundaries[0]
         $R = [long]$Boundaries[1]
-        $Answer1 += AddInvalidIds -Low $L -High $R
+        if ($R -gt $MaxValue) {
+            $MaxValue = $R
+        }
+        [array]$Pairs += New-Object psobject -Property @{
+            lower  = $L
+            upper = $R
+        }
+        $Answer1 += AddInvalidIds1 -Low $L -High $R
+    }
+
+    # Part2
+    [long]$Answer2 = 0
+    $finished = $False
+    $Set = New-Object System.Collections.Generic.HashSet[long]
+    for ([long]$i = 1; $i -le $MaxValue; $i++) {
+        $str = $i.toString()
+        $tempStr = $str
+        for ($j = 2; ($j * $str.Length) -le [Math]::Ceiling([Math]::Log10($MaxValue)); $j++) {
+            $tempStr += $str
+            $tempValue = [long]$tempStr
+            if ($tempValue -gt $MaxValue){
+                if ($j -eq 2) {
+                    $finished = $True
+                    break
+                } else {
+                    continue
+                }
+            }
+            # REMARK: Need to eliminate duplicates (e.g. "2" * 4 and "22" * 2).
+            $Set.Add($tempValue) | Out-Null
+        }
+        if ($finished) {
+            break
+        }
+    }
+    foreach ($v in $Set) {
+        foreach ($p in $Pairs) {
+            if($p.lower -le $v -and $p.upper -ge $v) {
+                $Answer2 += $v
+            }
+        }
     }
     
     Write-Host "Question 1: What do you get if you add up all of the invalid IDs?"
     Write-Host "Answer:", $Answer1
+    Write-Host "Question 2: What do you get if you add up all of the invalid IDs using these new rules?"
+    Write-Host "Answer:", $Answer2
 }
 
 Main
 
 # Question 1: What do you get if you add up all of the invalid IDs?
 # Answer: 31000881061
-
+# Question 2: What do you get if you add up all of the invalid IDs using these new rules?
+# Answer: 46769308485
