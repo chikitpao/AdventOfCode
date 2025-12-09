e2478e43a2cf5093bab34d11eb64509c017b538f
Chi-Kit Pao <Chi-KitPao@gmx.de>
2025-12-09 21:00:33 +0100

    Added Solution for AoC 2025, Day 8, Part 2

--- 2025/Day08/Day08.ps1
+++ 2025/Day08/Day08.ps1
@@ -10,6 +10,7 @@ class Box{
     [int]$X
     [int]$Y
     [int]$Z
+    [int]$Group
 }
 
 class Connection{
@@ -22,23 +23,19 @@ function GetDistance([Box]$Box1, [Box]$Box2) {
     return [Math]::sqrt([Math]::Pow($Box2.X - $Box1.X, 2) + [Math]::Pow($Box2.Y - $Box1.Y, 2) + [Math]::Pow($Box2.Z - $Box1.Z, 2))
 }
 
-function GetIndexInConnected($ConnectedBoxes, $UnconnectedBoxes, $BoxId) {
-    if($UnconnectedBoxes.Contains($BoxId)) {
+function GetIndexInConnected($Boxes, $ConnectedBoxes, $UnconnectedBoxes, $BoxId) {
+    if($Boxes[$BoxId].Group -eq -1) {
         $UnconnectedBoxes.Remove($BoxId) | Out-Null
         $ConnectedBoxes.Add([System.Collections.ArrayList]::new()) | Out-Null
+        $Boxes[$BoxId].Group = $ConnectedBoxes.Count - 1
         $ConnectedBoxes[-1].Add($BoxId) | Out-Null
         return $ConnectedBoxes.Count - 1
     } else {
-        for($i = 0; $i -lt $ConnectedBoxes.Count; $i++){
-            if ($ConnectedBoxes[$i].Contains($BoxId)) {
-                return $i
-            }
-        }
-        throw "Box ID not found!"
+        return $Boxes[$BoxId].Group
     }
 }
 
-function MergeConnections($ConnectedBoxes, $From, $To){
+function MergeConnections($Boxes, $ConnectedBoxes, $From, $To){
     if ($From -eq $To) {
         return
     }
@@ -50,12 +47,16 @@ function MergeConnections($ConnectedBoxes, $From, $To){
     }
     foreach ($Id in $ConnectedBoxes[$To]) {
         $ConnectedBoxes[$From].Add($Id) | Out-Null
+        $Boxes[$Id].Group = $From
     }
     $ConnectedBoxes[$To] = $Null
-    if($To -le ($ConnectedBoxes.Count - 1)) {
+    if($To -lt ($ConnectedBoxes.Count - 1)) {
         $ConnectedBoxes[$To] = $ConnectedBoxes[-1]
+        foreach ($BoxId in $ConnectedBoxes[$To]) {
+            $Boxes[$BoxId].Group = $To
+        }
     }
-    $ConnectedBoxes.Remove(($ConnectedBoxes[-1]))
+    $ConnectedBoxes.RemoveAt($ConnectedBoxes.Count - 1) | Out-Null
 }
 
 function Part1($Boxes, $SortedConnections, $NeededConnectionCount, $NeededResultCount){
@@ -64,11 +65,11 @@ function Part1($Boxes, $SortedConnections, $NeededConnectionCount, $NeededResult
     for($i = 0; $i -lt $Boxes.Count; $i++) {
         $UnconnectedBoxes.Add($i) | Out-Null
     }
-    for($i = 0; $i -lt $NeededConnectionCount; $i++){
+    for($i = 0; $i -lt $NeededConnectionCount; $i++) {
         $Connection = $SortedConnections[$i]
-        $FromIndexInConnected = (GetIndexInConnected -ConnectedBoxes $ConnectedBoxes -UnconnectedBoxes $UnconnectedBoxes -BoxId $Connection.From)
-        $ToIndexInConnected = (GetIndexInConnected -ConnectedBoxes $ConnectedBoxes -UnconnectedBoxes $UnconnectedBoxes -BoxId $Connection.To)
-        MergeConnections -ConnectedBoxes $ConnectedBoxes -From $FromIndexInConnected -To $ToIndexInConnected
+        $FromIndexInConnected = (GetIndexInConnected -Boxes $Boxes -ConnectedBoxes $ConnectedBoxes -UnconnectedBoxes $UnconnectedBoxes -BoxId $Connection.From)
+        $ToIndexInConnected = (GetIndexInConnected -Boxes $Boxes -ConnectedBoxes $ConnectedBoxes -UnconnectedBoxes $UnconnectedBoxes -BoxId $Connection.To)
+        MergeConnections -Boxes $Boxes -ConnectedBoxes $ConnectedBoxes -From $FromIndexInConnected -To $ToIndexInConnected
     }
 
     $BoxesCount = [System.Collections.ArrayList]::new()
@@ -86,6 +87,26 @@ function Part1($Boxes, $SortedConnections, $NeededConnectionCount, $NeededResult
     return $Result
 }
 
+function Part2($Boxes, $SortedConnections){
+    $ConnectedBoxes = [System.Collections.ArrayList]::new()
+    $UnconnectedBoxes = New-Object System.Collections.Generic.HashSet[int]
+    for($i = 0; $i -lt $Boxes.Count; $i++) {
+        $UnconnectedBoxes.Add($i) | Out-Null
+    }
+
+    $i = 0
+    do{
+        $Connection = $SortedConnections[$i]
+        $FromIndexInConnected = (GetIndexInConnected -Boxes $Boxes -ConnectedBoxes $ConnectedBoxes -UnconnectedBoxes $UnconnectedBoxes -BoxId $Connection.From)
+        $ToIndexInConnected = (GetIndexInConnected -Boxes $Boxes -ConnectedBoxes $ConnectedBoxes -UnconnectedBoxes $UnconnectedBoxes -BoxId $Connection.To)
+        MergeConnections -Boxes $Boxes -ConnectedBoxes $ConnectedBoxes -From $FromIndexInConnected -To $ToIndexInConnected
+        if($ConnectedBoxes.Count -eq 1 -and $UnconnectedBoxes.Count -eq 0) {
+            return $Boxes[$Connection.From].X * $Boxes[$Connection.To].X
+        }
+        $i++
+    } while ($True)
+}
+
 function Main {
     $CurrentId = 0
     $Boxes = [System.Collections.ArrayList]::new()
@@ -96,6 +117,7 @@ function Main {
         $Box.X = [int]$Coordinates[0]
         $Box.Y = [int]$Coordinates[1]
         $Box.Z = [int]$Coordinates[2]
+        $Box.Group = -1
         $Boxes.Add($Box) | Out-Null
         $CurrentId++
     }
@@ -114,9 +136,17 @@ function Main {
 
     Write-Host "Question 1: Connect together the 1000 pairs of junction boxes which are closest together. Afterward, what do you get if you multiply together the sizes of the three largest circuits?"
     Write-Host "Answer:", (Part1 -Boxes $Boxes -SortedConnections $SortedConnections -NeededConnectionCount 1000 -NeededResultCount 3)
+    
+    foreach ($Box in $Boxes) {
+        $Box.Group = -1
+    }
+    Write-Host "Question 2: Continue connecting the closest unconnected pairs of junction boxes together until they're all in the same circuit. What do you get if you multiply together the X coordinates of the last two junction boxes you need to connect?"
+    Write-Host "Answer:", (Part2 -Boxes $Boxes -SortedConnections $SortedConnections)
 }
 
 Main
 
 # Question 1: Connect together the 1000 pairs of junction boxes which are closest together. Afterward, what do you get if you multiply together the sizes of the three largest circuits?
 # Answer: 57564
+# Question 2: Continue connecting the closest unconnected pairs of junction boxes together until they're all in the same circuit. What do you get if you multiply together the X coordinates of the last two junction boxes you need to connect?
+# Answer: 133296744
